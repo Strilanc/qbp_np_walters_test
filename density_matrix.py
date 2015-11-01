35106d1cecb7a172c81a9c7dd00be9be9f5a980e
Craig Gidney <craig.gidney@gmail.com>
2015-11-01 01:39:18 -0400

    Defined most of the operations, but have not checked them all for corrections

--- density_matrix.py
+++ density_matrix.py
@@ -10,13 +10,7 @@ class DensityMatrix:
     """
 
     def __init__(self, state_matrix):
-        self.state = state_matrix
-
-    @staticmethod
-    def zeros(bit_count):
-        ρ = np.zeros((1 << bit_count, 1 << bit_count))
-        ρ[0, 0] = 1
-        return DensityMatrix(ρ)
+        self.mat = state_matrix
 
     def full_op(self, op_matrix):
         """
@@ -25,15 +19,16 @@ class DensityMatrix:
         :return: The post-operation state (as a density matrix).
         """
         # ρ₂ = U ρ U⁻¹
-        m = np.dot(np.dot(op_matrix, self.state), op_matrix.H)
+        m = np.dot(np.dot(op_matrix, self.mat), op_matrix.H)
         return DensityMatrix(m)
 
-    def qubit_op(self, op_matrix, qubit_index):
+    def qubit_op(self, op_matrix, qubit_index, control_bit=None):
         """
         Applies an operation to one of the qubits in the state, and returns
         the resulting state.
         :param: op_matrix A unitary matrix corresponding to the qubit operation.
         :param: qubit_index The qubit to apply the operation to.
+        :param: control_bit A qubit to condition on (using the Z axis).
         :return: The post-operation state (as a density matrix).
 
         >>> off = DensityMatrix(np.mat([[1, 0], [0, 0]]))
@@ -43,26 +38,33 @@ class DensityMatrix:
         >>> offoff = DensityMatrix(np.mat([[1,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]))
         >>> onoff = DensityMatrix(np.mat([[0,0,0,0],[0,1,0,0],[0,0,0,0],[0,0,0,0]]))
         >>> offon = DensityMatrix(np.mat([[0,0,0,0],[0,0,0,0],[0,0,1,0],[0,0,0,0]]))
-        >>> np.allclose(off.qubit_op(X, 0).state, on.state)
+        >>> np.allclose(off.qubit_op(X, 0).mat, on.mat)
         True
-        >>> np.allclose(on.qubit_op(X, 0).state, off.state)
+        >>> np.allclose(on.qubit_op(X, 0).mat, off.mat)
         True
-        >>> np.allclose(off.qubit_op(H, 0).state, np.mat([[0.5, 0.5], [0.5, 0.5]]))
+        >>> np.allclose(off.qubit_op(H, 0).mat, [[0.5, 0.5], [0.5, 0.5]])
         True
-        >>> np.allclose(on.qubit_op(H, 0).state, np.mat([[0.5, -0.5], [-0.5, 0.5]]))
+        >>> np.allclose(on.qubit_op(H, 0).mat, [[0.5, -0.5], [-0.5, 0.5]])
         True
-        >>> np.allclose(offoff.qubit_op(X, 0).state, onoff.state)
+        >>> np.allclose(offoff.qubit_op(X, 0).mat, onoff.mat)
         True
-        >>> np.allclose(offoff.qubit_op(X, 1).state, offon.state)
+        >>> np.allclose(offoff.qubit_op(X, 1).mat, offon.mat)
         True
         """
 
         # Use tensor product to expand operation to apply to full state.
         for i in range(qubit_index):
             op_matrix = np.kron(op_matrix, np.identity(2))
-        bit_count = int(round(math.log2(self.state.shape[0])))
+        bit_count = int(round(math.log2(self.mat.shape[0])))
         for i in range(bit_count - qubit_index - 1):
             op_matrix = np.kron(np.identity(2), op_matrix)
+        n = self.mat.shape[0]
+        if control_bit is not None:
+            for i in range(n):
+                for j in range(n):
+                    if (i & (1 << control_bit)) == 0 or \
+                       (j & (1 << control_bit)) == 0:
+                        op_matrix[i, j] = 1 if i == j else 0
 
         return self.full_op(op_matrix)
 
@@ -73,15 +75,15 @@ class DensityMatrix:
         :param bit_index: The index of the qubit to measure (along the Z axis).
         :return: A list of (result, probability, state) outcome tuples.
         """
-        n = self.state.shape[0]
+        n = self.mat.shape[0]
         projector_on = np.zeros((n, n))
         projector_off = np.zeros((n, n))
         for i in range(n):
             target = projector_on if i & (1 << bit_index) else projector_off
             target[i, i] = 1
 
-        off_state = np.dot(np.dot(projector_off, self.state), projector_off)
-        on_state = np.dot(np.dot(projector_on, self.state), projector_on)
+        off_state = np.dot(np.dot(projector_off, self.mat), projector_off)
+        on_state = np.dot(np.dot(projector_on, self.mat), projector_on)
 
         off_p = off_state.trace()[0, 0]
         on_p = on_state.trace()[0, 0]
@@ -105,22 +107,22 @@ class DensityMatrix:
         >>> hOff = DensityMatrix(np.mat([[1,1],[1,1]])/2)
         >>> hOn = DensityMatrix(np.mat([[1,-1],[-1,1]])/2)
         >>> coin = DensityMatrix(np.mat([[0.5,0],[0,0.5]]))
-        >>> np.allclose(hOff.measure_bit_ignore_result(0).state, coin.state)
+        >>> np.allclose(hOff.measure_bit_ignore_result(0).mat, coin.mat)
         True
-        >>> np.allclose(hOn.measure_bit_ignore_result(0).state, coin.state)
+        >>> np.allclose(hOn.measure_bit_ignore_result(0).mat, coin.mat)
         True
         """
         combined = functools.reduce(
-            lambda a, e: a + e[1] * e[2].state,
+            lambda a, e: a + e[1] * e[2].mat,
             self.measure_bit(bit_index),
-            np.zeros(self.state.shape))
+            np.zeros(self.mat.shape))
         return DensityMatrix(combined)
 
     def __str__(self):
-        return str(self.state)
+        return str(self.mat)
 
     def spectral_decomposition_str(self):
-        w, v = np.linalg.eig(self.state)
+        w, v = np.linalg.eig(self.mat)
         combo = []
         for i in range(len(w)):
             combo.append((w[i], v[:, i].transpose()))
@@ -129,4 +131,4 @@ class DensityMatrix:
         return " + ".join(desc)
 
     def __repr__(self):
-        return "DensityMatrix(" + repr(self.state) + ")"
+        return "DensityMatrix(" + repr(self.mat) + ")"
--- main.py
+++ main.py
@@ -16,6 +16,32 @@ Z = np.matrix([[1, 0], [0, -1]])
 H = np.matrix([[1, 1], [1, -1]])/math.sqrt(2)
 
 
+def matrix_lift(f):
+    """
+    Lifts a function to apply to a matrix. Works by taking the spectral
+    decomposition of the matrix, then applying the function to the eigenvalue
+    weights, then summing the matrix pieces back together with the new weights.
+    :param f: The number->number function to lift to a matrix->matrix function.
+    >>> sqrtm = matrix_lift(cmath.sqrt)
+    >>> np.allclose(sqrtm(X), np.mat([[1, -1j], [-1j, 1]]) * (1+1j)/2)
+    True
+    """
+
+    def matrix_lift_helper(m, f):
+        w, v = np.linalg.eig(m)
+        result = np.mat(np.zeros(m.shape, np.complex128))
+        for i in range(len(w)):
+            eigen_val = w[i]
+            eigen_vec = np.mat(v[:, i])
+            eigen_mat = np.dot(eigen_vec, eigen_vec.H)
+            result += f(eigen_val) * eigen_mat
+        return result
+
+    return lambda m: matrix_lift_helper(m, f)
+
+
+
+
 def op_op(op, f):
     """
     Applies an analytic operation to a single-qubit operation's matrix.
@@ -34,34 +60,158 @@ def op_pow(op, p):
     True
     >>> np.allclose(op_pow(X, -0.5), np.mat([[1, 1j], [1j, 1]]) * (1-1j)/2)
     True
-    >>> np.allclose(op_pow(Z, 0.5), np.mat([[1, 0], [0, 1j]]))
+    >>> np.allclose(op_pow(Z, 0.5), [[1, 0], [0, 1j]])
     True
-    >>> np.allclose(op_pow(Z, -0.5), np.mat([[1, 0], [0, -1j]]))
+    >>> np.allclose(op_pow(Z, -0.5), [[1, 0], [0, -1j]])
     True
     """
     return op_op(op, lambda e: e**p)
 
 
-def R(op, theta):
+def bloch_rot(op, θ):
     """
     Returns the standard 'rotate around bloch-sphere axis' operation, based on
     exponentiating the single-qubit operation.
 
-    >>> np.allclose(R(Y, π/2), np.mat([[1,-1],[1,1]])/math.sqrt(2))
+    >>> np.allclose(bloch_rot(Y, π/2), np.mat([[1,-1],[1,1]])/math.sqrt(2))
     True
-    >>> np.allclose(R(Y, -π/2), np.mat([[1,1],[-1,1]])/math.sqrt(2))
+    >>> np.allclose(bloch_rot(Y, -π/2), np.mat([[1,1],[-1,1]])/math.sqrt(2))
     True
-    >>> np.allclose(R(Y, π), np.mat([[0,-1],[1,0]]))
+    >>> np.allclose(bloch_rot(Y, π), [[0,-1],[1,0]])
     True
-    >>> np.allclose(R(Z, π), np.mat([[-1j,0],[0,1j]]))
+    >>> np.allclose(bloch_rot(Z, π), [[-1j,0],[0,1j]])
     True
-    >>> np.allclose(R(X, τ), np.mat([[-1,0],[0,-1]]))
+    >>> np.allclose(bloch_rot(X, τ), [[-1,0],[0,-1]])
     True
-    >>> np.allclose(R(Y, τ), np.mat([[-1,0],[0,-1]]))
+    >>> np.allclose(bloch_rot(Y, τ), [[-1,0],[0,-1]])
     True
-    >>> np.allclose(R(Z, τ), np.mat([[-1,0],[0,-1]]))
+    >>> np.allclose(bloch_rot(Z, τ), [[-1,0],[0,-1]])
     True
     """
-    return op_op(op, lambda e: cmath.exp(-1j * e * theta / 2))
+    return op_op(op, lambda e: cmath.exp(-1j * e * θ / 2))
 
 
+class Context:
+    def __init__(self, qubit_count):
+        self.qubit_count = qubit_count
+
+    def expand_op(self, qubit_op_matrix, qubit_index):
+        """
+        >>> np.allclose(Context(1).expand_op(X, 0), [[0,1],[1,0]])
+        True
+        >>> np.allclose(Context(2).expand_op(Z, 0), [[1,0,0,0],[0,-1,0,0],[0,0,1,0],[0,0,0,-1]])
+        True
+        >>> np.allclose(Context(2).expand_op(Z, 1), [[1,0,0,0],[0,1,0,0],[0,0,-1,0],[0,0,0,-1]])
+        True
+        """
+        post = np.identity(1 << qubit_index)
+        pre = np.identity(1 << (self.qubit_count - qubit_index - 1))
+        return np.kron(np.kron(pre, qubit_op_matrix), post)
+
+    def σx(self, n):
+        """
+        Returns a matrix that applies a Y gate to the n'th qubit.
+        :param n: Index of the qubit.
+        """
+        return self.expand_op(X, n)
+
+    def σy(self, n):
+        """
+        Returns a matrix that applies a Y gate to the n'th qubit.
+        :param n: Index of the qubit.
+        """
+        return self.expand_op(Y, n)
+
+    def σz(self, n):
+        """
+        Returns a matrix that applies a Y gate to the n'th qubit.
+        :param n: Index of the qubit.
+        """
+        return self.expand_op(Z, n)
+
+    def R(self, n, θ):
+        """
+        Applies a θ radian rotation around the Y axis to the n'th qubit.
+        :param n: The index of the qubit to rotate.
+        :param θ: The amount to rotate.
+        >>> s = math.sqrt(0.5)
+        >>> np.allclose(Context(1).R(0, π/2), [[s,s],[-s,s]])
+        True
+        >>> np.allclose(Context(2).R(0, π/2), [[s,s,0,0],[-s,s,0,0],[0,0,s,s],[0,0,-s,s]])
+        True
+        >>> np.allclose(Context(2).R(1, π/2), [[s,0,s,0],[0,s,0,s],[-s,0,s,0],[0,-s,0,s]])
+        True
+        """
+        return matrix_lift(lambda λ: cmath.exp(λ * 1j * θ / 2))(self.σy(n))
+
+
+    def Rc(self, n, m, i, θ, on):
+        """
+        :param n: Index of the control qubit.
+        :param m: Index of the target qubit.
+        :param i: Control axis.
+        :param θ: The amount to rotate.
+        :param on: Whether the control should be on vs off for things to happen.
+        """
+        f = matrix_lift(lambda λ: cmath.exp((-1 if on else 1) * λ * 1j * θ / 2))
+        c = self.σy(m)*self.expand_op(i, n)
+        return f(c)
+
+    def CROT(self, c, t, i, θ):
+        """
+        Controlled rotation of the t'th qubit around the Y axis, conditioned on
+        the c'th qubit along the given i axis.
+        :param c: Index of the control qubit.
+        :param t: Index of the target qubit.
+        :param i: The control axis.
+        :param θ: The amount to rotate by.
+        >>> s = math.sqrt(0.5)
+        >>> np.allclose(\
+                Context(2).CROT(1, 0, Z, π/2), \
+                [[1,0,0,0],\
+                 [0,1,0,0],\
+                 [0,0,s,s],\
+                 [0,0,-s,s]])
+        True
+        >>> np.allclose(\
+                Context(2).CROT(0, 1, Z, π/2), \
+                [[1,0,0,0],\
+                 [0,s,0,s],\
+                 [0,0,1,0],\
+                 [0,-s,0,s]])
+        True
+        >>> np.allclose(\
+                np.dot(Context(2).expand_op(H, 0), np.dot(Context(2).CROT(0, 1, X, π/2), Context(2).expand_op(H, 0))), \
+                [[1,0,0,0],\
+                 [0,s,0,s],\
+                 [0,0,1,0],\
+                 [0,-s,0,s]])
+        True
+        """
+        return self.R(t, θ/2) * self.Rc(c, t, i, θ/2, True)
+
+    def SCROTz(self, c, θ, s):
+        """
+        :param c: The target qubit.
+        :param θ: Amount to rotate by.
+        :param s: Index of a scratch qubit (initialized to zero).
+        """
+        Ps = self.expand_op(I, s)
+        return Ps * self.Rc(s, c, X, θ, True) * self.Rc(c, s, Z, π/2, True)
+
+    def SCROTx(self, c, θ, s):
+        """
+        :param c: The target qubit.
+        :param θ: Amount to rotate by.
+        :param s: Index of a scratch qubit (initialized to zero).
+        """
+        # Note: paper says the axis is `i = cos(φ) X + sin(φ) Z`
+        # but I think the X and Z were reversed by accident, since otherwise
+        # SCROTx would just be SCROTz
+        return self.R(c, π/2) * self.SCROTz(c, θ, s) * self.R(c, -π/2)
+
+    def QOR(self, c1, c2, s, s2):
+        return self.R(s, π) \
+            * self.SCROTx(s, π/2, s2) \
+            * self.CROT(c2, s, Z, π/2) \
+            * self.CROT(c1, s, Z, π/2)
